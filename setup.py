861f8f8c42eecd9064378ba7388d398e9bcd3e3f
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2017-11-21 23:59:26 -0500

    Added ability to open shadowrun maps

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2017.11.16"  # DONT FORGET TO UPDATE THE VERSION IN main.py
-version = (1, 5, 4)
+release_date = "2017.11.21"  # DONT FORGET TO UPDATE THE VERSION IN main.py
+version = (1, 6, 0)
 
 try:
     try:
