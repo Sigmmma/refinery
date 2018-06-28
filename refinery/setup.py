c51f2da8fd7b65d5e20030ab74dee96f2be74f58
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2018-06-28 02:00:17 -0400

    whoops

--- refinery/setup.py
+++ refinery/setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2018.06.05"  # DONT FORGET TO UPDATE THE VERSION IN main.py
-version = (1, 7, 8)
+release_date = "2018.06.28"  # DONT FORGET TO UPDATE THE VERSION IN main.py
+version = (1, 7, 9)
 
 try:
     try:
