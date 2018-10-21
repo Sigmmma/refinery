aeb2b32403f7043c453a6995db32f13cb392fdea
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2018-10-21 03:31:38 -0400

    Working on filename shortening

--- recursive_rename/tag_path_handler.py
+++ recursive_rename/tag_path_handler.py
@@ -2,6 +2,9 @@ from refinery.recursive_rename.constants import *
 from refinery.recursive_rename.functions import sanitize_path
 from supyr_struct.defs.util import str_to_identifier
 
+from os.path import splitext
+from queue import LifoQueue, Empty as EmptyQueueException
+
 
 class TagPathHandler():
     _path_map = ()
@@ -154,12 +157,15 @@ class TagPathHandler():
         self._priorities[index] = float(priority)
 
     def shorten_paths(self, max_len):
-        sorted_paths = {}
+        paths = {}
+        new_paths = {}
 
-        for tag_path in self._path_map:
-            curr_dir = sorted_paths
+        for tag_path, index in self._path_map.items():
+            curr_dir = paths
             tag_path_pieces = tag_path.split("\\")
-            if len(tag_path_pieces)*2 - 1 > max_len:
+            # 1 char for \, 1 for potential ~, 1 for potential number,
+            # and 1 for at least one name character
+            if (len(tag_path_pieces) - 1)*4 > max_len:
                 raise ValueError("Tag paths too nested to shorten to %s chars."
                                  % max_len)
                 return
@@ -167,5 +173,149 @@ class TagPathHandler():
             for dirname in tag_path_pieces[: -1]:
                 curr_dir = curr_dir.setdefault(dirname, {})
 
-            curr_dir[tag_path_pieces[-1]] = self._path_map[tag_path]
+            curr_dir[tag_path_pieces[-1]] = self._index_map[index]
 
+        # do a preliminary filepath shortening by removing any
+        # words the directories and paths start and end with
+        # that the parent directory also starts or ends with.
+        stack = LifoQueue()
+        parent = ""
+        curr_paths = paths
+        curr_new_paths = new_paths
+        reparent = {}
+
+
+
+        # TODO: FINISH THIS SHIT
+
+
+        while True:
+            for name in sorted(curr_paths):
+                val = curr_paths[name]
+                if not isinstance(val, dict):
+                    # reached a filename. move the item and continue.
+                    basename, ext = splitext(name)
+                    new_basename = self.shorten_name_to_parent(parent, basename)
+                    curr_paths.pop(name)
+                    if new_basename:
+                        curr_new_paths[new_basename + ext] = val
+                    else:
+                        # name was simplified to nothing.
+                        # schedule it to be put it in the parent
+                        reparent.setdefault(parent, []).append(val)
+
+                elif val:
+                    # need to go deeper, as this is a non-empty
+                    # directory. store the current state to
+                    # the stack and jump into this dir.
+                    stack.put([parent, curr_paths, curr_new_paths, reparent])
+                    new_name = self.get_unique_name(
+                        curr_new_paths,
+                        self.shorten_name_to_parent(parent, name)
+                        )
+                    if new_name:
+                        # if the name doesn't get simplified to nothing,
+                        # create a new directory to store these items in
+                        # CANNOT SIMPLIFY THIS TO THIS
+                        #    curr_new_paths = curr_new_paths[name] = {}
+                        parent = name
+                        if new_name not in curr_new_paths:
+                            curr_new_paths[new_name] = {}
+
+                        curr_new_paths = curr_new_paths[new_name]
+
+                    curr_paths = val
+                    reparent = {}
+                    break
+                else:
+                    # this is an empty directory(already copied all file
+                    # and directory entries from it). remove it.
+                    curr_paths.pop(name)
+
+
+            if curr_paths:
+                # still have paths to copy.
+                continue
+
+
+            # re-parent any paths that needed to be reparented
+            for name in reparent:
+                for item in reparent[name]:
+                    curr_new_paths[
+                        self.get_unique_name(curr_new_paths,
+                                             *splitext(name))] = item
+
+
+            reparent.clear()
+            try:
+                # exhausted the current paths, get the next ones to do
+                parent, curr_paths, curr_new_paths, reparent = stack.get_nowait()
+            except EmptyQueueException:
+                break
+
+
+        # apply the renames
+        curr_paths = new_paths
+        path_pieces = ()
+        while True:
+            for name in sorted(curr_paths):
+                val = curr_paths.pop(name)
+                if not isinstance(val, dict):
+                    # reached a filename. rename the item and continue.
+                    if not val.indexed:
+                        val.tag.tag_path = "\\".join(
+                            path_pieces + splitext(name)[: 1])
+
+                    continue
+                elif val:
+                    # This is a non-empty directory. Need to go
+                    # deeper, so store the current state to the
+                    # stack and jump into this dir.
+                    stack.put([path_pieces, curr_paths])
+
+                    curr_paths = val
+                    path_pieces += (name, )
+
+                break
+
+            if curr_paths:
+                # still have paths to modify.
+                continue
+
+            try:
+                # exhausted the current paths, get the next ones to do
+                path_pieces, curr_paths = stack.get_nowait()
+            except EmptyQueueException:
+                break
+
+
+        # remake the path map
+        self._path_map.clear()
+        for i in range(len(self._index_map)):
+            ref = self._index_map[i]
+            self._path_map[('%s.%s' % (
+                ref.tag.tag_path, ref.class_1.enum_name)).lower()] = i
+
+    def shorten_name_to_parent(self, parent, name):
+        join_char = '_' if '_' in name else ' '
+        parent_pieces = parent.replace('_', ' ').split(' ')
+        name_pieces = name.replace('_', ' ').split(' ')
+        start, end = 0, len(name_pieces)
+        for i in range(min(len(parent_pieces), len(name_pieces))):
+            if parent_pieces[i] == name_pieces[i]:
+                start += 1
+
+        for i in range(min(len(parent_pieces), len(name_pieces))):
+            if parent_pieces[-1 - i] == name_pieces[-1 - i]:
+                end -= 1
+
+        return join_char.join(name_pieces[start: end])
+
+    def get_unique_name(self, collection, name, ext=""):
+        if name + ext not in collection:
+            return name + ext
+
+        i = 1
+        while "%s %s%s" % (name, i, ext) in collection:
+            i += 1
+        return "%s %s%s" % (name, i, ext)
