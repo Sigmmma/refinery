3a99110bdcd2ae7eb595794b22b8fbbcc2d6aa17
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2019-03-21 16:24:54 -0400

    Version bump Fixed bug with infinite priority sneaking its way into protected names Better handling of actv weapon and dropped item priority Fixed bug with choosing unique name for tags not incrementing number

--- refinery/__init__.py
+++ refinery/__init__.py
@@ -3,8 +3,8 @@
 # ##############
 __author__ = "Devin Bobadilla"
 #           YYYY.MM.DD
-__date__ = "2019.02.16"
-__version__ = (2, 0, 6)
+__date__ = "2019.03.21"
+__version__ = (2, 1, 0)
 __all__ = (
     'hashcacher', 'hashcacher_window', 'main', 'meta_window', 'widgets', 'crc_functions'
     )
