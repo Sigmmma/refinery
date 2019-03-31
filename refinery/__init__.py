b63a68fc44f2744982b8f7f1a8435d60c4b09384
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2019-03-31 17:25:46 -0400

    Version bump

--- refinery/__init__.py
+++ refinery/__init__.py
@@ -3,8 +3,8 @@
 # ##############
 __author__ = "Devin Bobadilla"
 #           YYYY.MM.DD
-__date__ = "2019.03.25"
-__version__ = (2, 1, 1)
+__date__ = "2019.03.31"
+__version__ = (2, 1, 2)
 __all__ = (
     'hashcacher', 'hashcacher_window', 'main', 'meta_window', 'widgets', 'crc_functions'
     )
