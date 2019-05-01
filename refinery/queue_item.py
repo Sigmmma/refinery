5e7ff1a8e4991a4e1107182ac5a293c696e82f02
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2019-05-01 02:08:46 -0400

    More splitting Added several queueable methods Fixed bugs with setting map Implemented experimental speed-up to heuristic deprotection Fixed bug with scraping tag paths from scripts not accounting for globals flag

--- refinery/queue_item.py
+++ refinery/queue_item.py
@@ -1,100 +1,80 @@
-
-from refinery.tag_index_crawler import TagIndexCrawler
+import os
 
 
 class RefineryQueueItem:
-    # TODO: consider moving these into optional kwargs in op_args
-    recursive = False
-    overwrite = False
-    tagslist_path = ""
-    out_dir = ""
+    __slots__ = ("_op", "_op_kwargs")
 
-    engine_name = ""
-    map_name = ""
+    def __init__(self, operation, **kwargs):
+        op = str(operation)
+        provided_kwargs = set(kwargs)
 
-    _op = "extract_tags"
-    _op_args = ()
-    _op_kwargs = ()
+        if op == "extract_cheape":
+            required = ("filepath", )
+            if "filepath" not in kwargs and ("out_dir" in kwargs and
+                                             "map_name" in kwargs):
+                kwargs["filepath"] = os.path.join(
+                    self.out_dir, self.map_name + "_cheape.map")
 
-    _valid_ops = frozenset((
-        # each of these operation types requires specific numbers
-        # and types of arguments(op_args) to be provided to work.
-        
-        "extract_tags", # first arg is an iterable of any mix of tag ids,
-        #                 paths, or file extensions. if extensions, ensure
-        #                 everything before the period(.) is an asterisk(*).
-        "extract_data", # same as extract_tags.
-        "extract_cheape", # first arg is the filepath to save cheape to. if not
-        #                   provided, defaults to out_dir + halo_map.map_name
-        "deprotect", # first arg is the filepath to save the deprotected map to.
-        "load_map", # first arg is the filepath to the map to load.
-        "save_map", # first arg is the filepath to save the map to.
-        #             optional second arg is a 2-tuple specifying which map
-        #             to save as (engine_name, map_name).
+        elif op in ("load_map", "deprotect", "save_map"):
+            required = ("filepath", )
 
-        # NOTE: These operations require a separate save_map operation
-        #       to be done before their changes will be saved anywhere.
-        "crc_spoof",  # first arg is the crc to spoof the map with. can be
-        #               provided as either an int, or a length 4 byte string.
-        #               optional second arg is a 2-tuple specifying which map
-        #               to spoof (engine_name, map_name).
-        "rename_map", # first arg is the new name of the map. must be a valid
-        #               filename, and must be less than 32 characters.
-        #               optional second arg is a 2-tuple specifying which map
-        #               to rename as (engine_name, map_name).
-        "rename_tag", # first arg is an iterable of 2-tuples specifying the
-        #               source and destination for each rename as (src, dst)
-        #               optional second arg is a 2-tuple specifying which map
-        #               to rename as (engine_name, map_name).
-        "rename_dir", # same as rename_tag.
-        ))
-    def __init__(self, operation, operation_args, **kwargs):
-        self.recursive = kwargs.pop("recursive", self.recursive)
-        self.overwrite = kwargs.pop("overwrite", self.overwrite)
-        self.tagslist_path = kwargs.pop("tagslist_path", self.tagslist_path)
-        self.out_dir = kwargs.pop("out_dir", self.out_dir)
+        elif op in ("extract_tags", "extract_data"):
+            required = ("tag_ids", )
 
-        self.engine_name = kwargs.pop("engine_name", self.engine_name)
-        self.map_name = kwargs.pop("map_name", self.map_name)
-
-        self._op = str(operation)
-        if isinstance(operation_args, str):
-            self._op_args = (operation_args, )
+        elif op == "unload_map": pass
+        elif op == "extract_tag": required = ("tag_id", )
+        elif op == "switch_map":  required = ("map_name", )
+        elif op == "switch_engine": required = ("engine_name", )
+        elif op == "spoof_map_crc": required = ("new_crc", )
+        elif op == "set_default": required = ("name", "value")
+        elif op == "rename_map": required = ("new_name", )
+        elif op == "rename_tag_by_id": required = ("tag_id", "new_path")
+        elif op == "rename_tag": required = ("tag_path", "new_path")
+        elif op == "rename_dir": required = ("dir_path", "new_path")
         else:
-            self._op_args = tuple(operation_args)
-            
-        self._op_kwargs = tuple(kwargs)
+            raise ValueError('The hell kind of operation is "%s"' % op)
 
-        # fill these out for determining vailidity of operation_args
-        # also ensure all unprovided args are set to blank defaults
-        if operation in ("extract_tags", "extract_data"):
-            pass
-        elif operation == "extract_cheape":
-            pass
-        elif operation == ("deprotect", "load_map"):
-            pass
-        elif operation == "save_map":
-            pass
-        elif operation == "crc_spoof":
-            pass
-        elif operation == "rename_map":
-            pass
-        elif operation in ("rename_tag", "rename_dir"):
-            pass
+        missing_kwargs = set(required).difference(provided_kwargs)
+
+        if missing_kwargs:
+            raise KeyError(
+                'The following keyword arguments are required for '
+                'the "%s" operation, but were not supplied:\n%s' %
+                (op, tuple(sorted(missing_kwargs))))
+
+        assert isinstance(kwargs.get("filepath", ""), str)
+        assert isinstance(kwargs.get("out_dir", ""), str)
+        assert isinstance(kwargs.get("new_name", ""), str)
+        assert isinstance(kwargs.get("map_name", ""), str)
+        assert isinstance(kwargs.get("engine_name", ""), str)
+        assert isinstance(kwargs.get("new_crc", 0), int)
+        assert isinstance(kwargs.get("tagslist_path", ""), str)
+        assert isinstance(kwargs.get("bitmap_extract_format", ""), str)
+        assert isinstance(kwargs.get("tag_id", ""), (int, str))
+        assert isinstance(kwargs.get("tag_ids", ()), (tuple, list, set))
+        assert isinstance(kwargs.get("tag_path", ""), str)
+        assert isinstance(kwargs.get("dir_path", ""), str)
+        assert isinstance(kwargs.get("new_path", ""), str)
+
+        assert kwargs.get("new_crc", 0) in range(0, 0x100000000)
+        assert kwargs.get("name", " ")[0] != "_" # disallow setting private variables
+
+        self._op = op
+        self._op_kwargs = kwargs
+
+    def __getattribute__(self, attr_name):
+        try:
+            return object.__getattribute__(self, attr_name)
+        except AttributeError:
+            return self.operation_kwargs[attr_name]
 
     def __setattr__(self, attr_name, new_val):
-        if attr_name == "_op":
-            assert new_val in self._valid_ops
-        object.__setattr__(self, attr_name, new_val)
+        try:
+            object.__setattr__(self, attr_name, new_val)
+        except AttributeError:
+            self.operation_kwargs[attr_name] = new_val
 
     @property
     def operation(self): return self._op
     @property
-    def operation_args(self): return self._op_args
-    @property
-    def operation_kwargs(self): return self._op_kwargs
-
-    def get_filtered_tag_ids(self, halo_map):
-        if self.operation in ("extract_tags", "extract_data"):
-            return TagIndexCrawler(self.operation_args).get_filtered_tag_ids(halo_map)
-        return ()
+    def operation_kwargs(self): return dict(self._op_kwargs)
