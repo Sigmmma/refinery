35b31ab43267b577bc325069d695932bbe97bbc5
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2019-05-10 02:21:19 -0400

    Adding more help strings

--- repl_arg_parsers.py
+++ repl_arg_parsers.py
@@ -24,72 +24,72 @@ for name in sorted((
         "dir", "files", "dir_ct", "file_ct", "dir_names", "file_names",
         "quit", "maps", "engines", "verbose", "prompt", "cls")):
     _ops[name] = repl_subparser.add_parser(
-        name.replace("_", "-"), help=command_help_strings.get(name))
+        name.replace("_", "-"), help=command_help_strings[name])
 
 
 #########################################################################
 # add the positional arguments
 #########################################################################
 _ops["rename_tag"].add_argument(
-    'tag-path', help=command_arg_strings["rename_tag"].get('tag-path'))
+    'tag-path', help=command_arg_strings["rename_tag"]['tag-path'])
 
 _ops["rename_dir"].add_argument(
-    'dir-path', help=command_arg_strings["rename_dir"].get('dir-path'))
+    'dir-path', help=command_arg_strings["rename_dir"]['dir-path'])
 
 _ops["prompt"].add_argument(
-    'level', default=False, choices=(0, 1, 2), type=int,
-    help=command_arg_strings["prompt"].get('level'))
+    'level', default=None, choices=(0, 1, 2), type=int, nargs="?",
+    help=command_arg_strings["prompt"]['level'])
 
 _ops["verbose"].add_argument(
-    'level', default=False, type=int,
-    help=command_arg_strings["verbose"].get('level'))
+    'level', default=None, type=int, nargs="?",
+    help=command_arg_strings["verbose"]['level'])
     
 _ops["maps"].add_argument(
     'engine', default=None, nargs="?",
-    help=command_arg_strings["maps"].get('engine'))
+    help=command_arg_strings["maps"]['engine'])
 
 _ops["rename_map"].add_argument(
-    'new-name', help=command_arg_strings["rename_map"].get('new-name'))
+    'new-name', help=command_arg_strings["rename_map"]['new-name'])
 
 _ops["switch_map"].add_argument(
-    'map-name', help=command_arg_strings["switch_map"].get('map-name'))
+    'map-name', help=command_arg_strings["switch_map"]['map-name'])
 
 _ops["switch_engine"].add_argument(
-    'engine', help=command_arg_strings["switch_engine"].get('engine'))
+    'engine', help=command_arg_strings["switch_engine"]['engine'])
 
 _ops["spoof_crc"].add_argument(
-    'new-crc', type=int, help=command_arg_strings["spoof_crc"].get('new-crc'))
+    'new-crc', type=int, help=command_arg_strings["spoof_crc"]['new-crc'])
 
 _ops["tag_id_tokens"].add_argument(
     'token-prefix', default="", nargs="?",
-    help=command_arg_strings["tag_id_tokens"].get('token-prefix'))
+    help=command_arg_strings["tag_id_tokens"]['token-prefix'])
 
 _ops["tag_id_macros"].add_argument(
     'macro-prefix', default="", nargs="?",
-    help=command_arg_strings["tag_id_macros"].get('macro-prefix'))
+    help=command_arg_strings["tag_id_macros"]['macro-prefix'])
 
 for name in ("load_map", "extract_cheape", "switch_map_by_filepath"):
     _ops[name].add_argument(
-        'filepath', help=command_arg_strings[name].get('filepath'))
+        'filepath', help=command_arg_strings[name]['filepath'])
 
 for name in ("deprotect_map", "save_map"):
     _ops[name].add_argument(
         'filepath', default=None, nargs="?",
-        help=command_arg_strings[name].get('filepath'))
+        help=command_arg_strings[name]['filepath'])
 
 for name in ("dir", "files", "dir_ct", "file_ct",
              "dir_names", "file_names"):
     _ops[name].add_argument(
         'dir', default=None, nargs="?",
-        help=command_arg_strings[name].get('dir'))
+        help=command_arg_strings[name]['dir'])
 
 for name in ("rename_tag_by_id", "extract_tag"):
     _ops[name].add_argument(
-        'tag-id', help=command_arg_strings[name].get('tag-id'))
+        'tag-id', help=command_arg_strings[name]['tag-id'])
 
 for name in ("rename_tag_by_id", "rename_tag", "rename_dir"):
     _ops[name].add_argument(
-        'new-path', help=command_arg_strings[name].get('new-path'))
+        'new-path', help=command_arg_strings[name]['new-path'])
 
 
 # most args accept map-name and engine as their last optional positional args
@@ -101,10 +101,10 @@ for name in (
         "dir_names", "file_names"):
     _ops[name].add_argument(
         'map-name', default=None, nargs="?",
-        help=command_arg_strings[name].get('map-name'))
+        help=command_arg_strings[name]['map-name'])
     _ops[name].add_argument(
         'engine', default=None, nargs="?",
-        help=command_arg_strings[name].get('engine'))
+        help=command_arg_strings[name]['engine'])
 
 
 #########################################################################
@@ -113,85 +113,85 @@ for name in (
 for name in ("deprotect_map", "load_map"):
     _ops[name].add_argument(
         '--do-printout', default=None, type=int,
-        help=command_arg_strings[name].get('do-printout'))
+        help=command_arg_strings[name]['do-printout'])
 
 for name in ("deprotect_map", "save_map"):
     _ops[name].add_argument(
         '--fix-tag-index-offset', default=None, type=int,
-        help=command_arg_strings[name].get('fix-tag-index-offset'))
+        help=command_arg_strings[name]['fix-tag-index-offset'])
     _ops[name].add_argument(
         '--meta-data-expansion', default=None, type=int,
-        help=command_arg_strings[name].get('meta-data-expansion'))
+        help=command_arg_strings[name]['meta-data-expansion'])
     _ops[name].add_argument(
         '--raw-data-expansion', default=None, type=int,
-        help=command_arg_strings[name].get('raw-data-expansion'))
+        help=command_arg_strings[name]['raw-data-expansion'])
     _ops[name].add_argument(
         '--triangle-data-expansion', default=None, type=int,
-        help=command_arg_strings[name].get('triangle-data-expansion'))
+        help=command_arg_strings[name]['triangle-data-expansion'])
     _ops[name].add_argument(
         '--vertex-data-expansion', default=None, type=int,
-        help=command_arg_strings[name].get('vertex-data-expansion'))
+        help=command_arg_strings[name]['vertex-data-expansion'])
 
 for name in ("dir_ct", "file_ct"):
     _ops[name].add_argument(
         '-t', '--total', default=False, action="store_const", const=True,
-        dest="total", help=command_arg_strings[name].get('total'))
+        dest="total", help=command_arg_strings[name]['total'])
 
 for name in ("extract_tags", "extract_data", "deprotect_map"):
     _ops[name].add_argument(
         '--print-errors', default=None, type=int,
-        help=command_arg_strings[name].get('print-errors'))
+        help=command_arg_strings[name]['print-errors'])
 
 for name in ("dir", "files"):
     _ops[name].add_argument(
         '--depth', default=None, type=int,
-        help=command_arg_strings[name].get('depth'))
+        help=command_arg_strings[name]['depth'])
     _ops[name].add_argument(
         '--guides', default=None, type=int,
-        help=command_arg_strings[name].get('guides'))
+        help=command_arg_strings[name]['guides'])
     _ops[name].add_argument(
         '--header', default=None, type=int,
-        help=command_arg_strings[name].get('header'))
+        help=command_arg_strings[name]['header'])
     _ops[name].add_argument(
         '--indexed', default=None, type=int,
-        help=command_arg_strings[name].get('indexed'))
+        help=command_arg_strings[name]['indexed'])
     _ops[name].add_argument(
         '--tag-ids', default=None, type=int,
-        help=command_arg_strings[name].get('tag-ids'))
+        help=command_arg_strings[name]['tag-ids'])
 
 for name in ("extract_tag", "extract_tags", "extract_data"):
     _ops[name].add_argument(
         '--force-lower-case-paths', default=None, type=int,
-        help=command_arg_strings[name].get('force-lower-case-paths'))
+        help=command_arg_strings[name]['force-lower-case-paths'])
     _ops[name].add_argument(
         '--generate-comp-verts', default=None, type=int,
-        help=command_arg_strings[name].get('generate-comp-verts'))
+        help=command_arg_strings[name]['generate-comp-verts'])
     _ops[name].add_argument(
         '--generate-uncomp-verts', default=None, type=int,
-        help=command_arg_strings[name].get('generate-uncomp-verts'))
+        help=command_arg_strings[name]['generate-uncomp-verts'])
     _ops[name].add_argument(
         '--out-dir', default=None,
-        help=command_arg_strings[name].get('out-dir'))
+        help=command_arg_strings[name]['out-dir'])
     _ops[name].add_argument(
         '--overwrite', default=None, type=int,
-        help=command_arg_strings[name].get('overwrite'))
+        help=command_arg_strings[name]['overwrite'])
     _ops[name].add_argument(
         '--recursive', default=None, type=int,
-        help=command_arg_strings[name].get('recursive'))
+        help=command_arg_strings[name]['recursive'])
     _ops[name].add_argument(
         '--rename-scnr-dups', default=None, type=int,
-        help=command_arg_strings[name].get('rename-scnr-dups'))
+        help=command_arg_strings[name]['rename-scnr-dups'])
     _ops[name].add_argument(
         '--tagslist-path', default=None,
-        help=command_arg_strings[name].get('tagslist-path'))
+        help=command_arg_strings[name]['tagslist-path'])
 
 for name in ("extract_tags", "extract_data"):
     _ops[name].add_argument(
         '--tokens', default=True, type=int,
-        help=command_arg_strings[name].get('tokens'))
+        help=command_arg_strings[name]['tokens'])
     _ops[name].add_argument(
         '--macros', default=True, type=int,
-        help=command_arg_strings[name].get('macros'))
+        help=command_arg_strings[name]['macros'])
 
 for name in (
         "autoload-resources", "do-printout", "print-errors",
@@ -205,12 +205,12 @@ for name in (
         "use-heuristics", "shallow-ui-widget-nesting", "rename-cached-tags"):
     _ops["set_vars"].add_argument(
         '--%s' % name, default=None, type=int,
-        help=command_arg_strings["set_vars"].get(name))
+        help=command_arg_strings["set_vars"][name])
 
 for name in ("tags-dir", "data-dir", "tagslist-path", "bitmap-extract-format"):
     _ops["set_vars"].add_argument(
         '--%s' % name, default=None,
-        help=command_arg_strings["set_vars"].get(name))
+        help=command_arg_strings["set_vars"][name])
     
 for name in (
         "autoload-resources", "do-printout", "print-errors",
@@ -225,80 +225,80 @@ for name in (
         "tags-dir", "data-dir", "tagslist-path", "bitmap-extract-format"):
     _ops["get_vars"].add_argument(
         '--%s' % name, const=True, default=False, action="store_const",
-        help=command_arg_strings["get_vars"].get(name))
+        help=command_arg_strings["get_vars"][name])
 
 for name in ("extract_tags", "extract_data"):
     _ops[name].add_argument(
         '--tag-ids', nargs="*", default=(tag_path_tokens.TOKEN_ALL, ),
-        help=command_arg_strings[name].get('tag-ids'))
+        help=command_arg_strings[name]['tag-ids'])
 
 _ops["extract_tag"].add_argument(
     '--filepath', default=None,
-    help=command_arg_strings["extract_tag"].get('filepath'))
+    help=command_arg_strings["extract_tag"]['filepath'])
 
 _ops["extract_data"].add_argument(
     '--decode-adpcm', default=None, type=int,
-    help=command_arg_strings["extract_data"].get('decode-adpcm'))
+    help=command_arg_strings["extract_data"]['decode-adpcm'])
 _ops["extract_data"].add_argument(
     '--bitmap-extract-format', default=None, choices=["dds", "tga", "png"],
-    help=command_arg_strings["extract_data"].get('bitmap-extract-format'))
+    help=command_arg_strings["extract_data"]['bitmap-extract-format'])
 _ops["extract_data"].add_argument(
     '--bitmap-extract-keep-alpha', default=None, type=int,
-    help=command_arg_strings["extract_data"].get('bitmap-extract-keep-alpha'))
+    help=command_arg_strings["extract_data"]['bitmap-extract-keep-alpha'])
 
 _ops["load_map"].add_argument(
     '--autoload-resources', default=None, type=int,
-    help=command_arg_strings["load_map"].get('autoload-resources'))
+    help=command_arg_strings["load_map"]['autoload-resources'])
 _ops["load_map"].add_argument(
     '--make-active', default=None, type=int,
-    help=command_arg_strings["load_map"].get('make-active'))
+    help=command_arg_strings["load_map"]['make-active'])
 _ops["load_map"].add_argument(
     '--replace-if-same-name', default=None, type=int,
-    help=command_arg_strings["load_map"].get('replace-if-same-name'))
+    help=command_arg_strings["load_map"]['replace-if-same-name'])
 
 _ops["dir"].add_argument(
     '--dirs-first', default=None, type=int,
-    help=command_arg_strings["dir"].get('dirs-first'))
+    help=command_arg_strings["dir"]['dirs-first'])
 _ops["dir"].add_argument(
     '--extra-dir-spacing', default=None, type=int,
-    help=command_arg_strings["dir"].get('extra-dir-spacing'))
+    help=command_arg_strings["dir"]['extra-dir-spacing'])
 _ops["dir"].add_argument(
     '--files', default=None, type=int,
-    help=command_arg_strings["dir"].get('files'))
+    help=command_arg_strings["dir"]['files'])
 _ops["dir"].add_argument(
     '--indent', default=None, type=int,
-    help=command_arg_strings["dir"].get('indent'))
+    help=command_arg_strings["dir"]['indent'])
 
 _ops["deprotect_map"].add_argument(
     '--fix-tag-classes', default=None, type=int,
-    help=command_arg_strings["deprotect_map"].get('fix-tag-classes'))
+    help=command_arg_strings["deprotect_map"]['fix-tag-classes'])
 _ops["deprotect_map"].add_argument(
     '--limit-tag-path-lengths', default=None, type=int,
-    help=command_arg_strings["deprotect_map"].get('limit-tag-path-lengths'))
+    help=command_arg_strings["deprotect_map"]['limit-tag-path-lengths'])
 _ops["deprotect_map"].add_argument(
     '--print-heuristic-name-changes', default=None, type=int,
-    help=command_arg_strings["deprotect_map"].get('print-heuristic-name-changes'))
+    help=command_arg_strings["deprotect_map"]['print-heuristic-name-changes'])
 _ops["deprotect_map"].add_argument(
     '--rename-cached-tags', default=None, type=int,
-    help=command_arg_strings["deprotect_map"].get('rename-cached-tags'))
+    help=command_arg_strings["deprotect_map"]['rename-cached-tags'])
 _ops["deprotect_map"].add_argument(
     '--scrape-tag-paths-from-scripts', default=None, type=int,
-    help=command_arg_strings["deprotect_map"].get('scrape-tag-paths-from-scripts'))
+    help=command_arg_strings["deprotect_map"]['scrape-tag-paths-from-scripts'])
 _ops["deprotect_map"].add_argument(
     '--shallow-ui-widget-nesting', default=None, type=int,
-    help=command_arg_strings["deprotect_map"].get('shallow-ui-widget-nesting'))
+    help=command_arg_strings["deprotect_map"]['shallow-ui-widget-nesting'])
 _ops["deprotect_map"].add_argument(
     '--use-heuristics', default=None, type=int,
-    help=command_arg_strings["deprotect_map"].get('use-heuristics'))
+    help=command_arg_strings["deprotect_map"]['use-heuristics'])
 _ops["deprotect_map"].add_argument(
     '--use-minimum-priorities', default=None, type=int,
-    help=command_arg_strings["deprotect_map"].get('use-minimum-priorities'))
+    help=command_arg_strings["deprotect_map"]['use-minimum-priorities'])
 _ops["deprotect_map"].add_argument(
     '--use-scenario-names-for-script-names', default=None, type=int,
-    help=command_arg_strings["deprotect_map"].get('use-scenario-names-for-script-names'))
+    help=command_arg_strings["deprotect_map"]['use-scenario-names-for-script-names'])
 _ops["deprotect_map"].add_argument(
     '--use-tag-index-for-script-names', default=None, type=int,
-    help=command_arg_strings["deprotect_map"].get('use-tag-index-for-script-names'))
+    help=command_arg_strings["deprotect_map"]['use-tag-index-for-script-names'])
 _ops["deprotect_map"].add_argument(
     '--valid-tag-paths-are-accurate', default=None, type=int,
-    help=command_arg_strings["deprotect_map"].get('valid-tag-paths-are-accurate'))
+    help=command_arg_strings["deprotect_map"]['valid-tag-paths-are-accurate'])
