bc6256f63b53f85316cc27deaf63f8943d36cb96
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2019-06-13 23:19:27 -0400

    Changed how read/write access is handled for maps

--- __init__.py
+++ __init__.py
@@ -3,8 +3,8 @@
 # ##############
 __author__ = "Devin Bobadilla"
 #           YYYY.MM.DD
-__date__ = "2019.05.29"
-__version__ = (2, 1, 4)
+__date__ = "2019.06.14"
+__version__ = (2, 1, 5)
 __all__ = (
     'core', 'crc_functions', 'hashcacher', 'main',
     'hashcacher_window', 'meta_window', 'widgets',
