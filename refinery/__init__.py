aa71e3b44e7cbb586d89be2fd5762aaa9833533f
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2019-08-04 16:52:27 -0400

    Version bump

--- refinery/__init__.py
+++ refinery/__init__.py
@@ -3,8 +3,8 @@
 # ##############
 __author__ = "Devin Bobadilla"
 #           YYYY.MM.DD
-__date__ = "2019.07.05"
-__version__ = (2, 2, 0)
+__date__ = "2019.08.04"
+__version__ = (2, 2, 1)
 __all__ = (
     'defs', 'heuristic_deprotection', 'repl', 'tag_index', 'widgets', 'windows',
     'constants', 'core', 'crc_functions', 'exceptions', 'main',
