f5f928ff4c14ea92d4325d696c681e0d9147e748
Moses_of_Egypt <mosesbobadilla@gmail.com>
2019-09-05 14:54:48 -0400

    Version bump

--- __init__.py
+++ __init__.py
@@ -3,8 +3,8 @@
 # ##############
 __author__ = "Devin Bobadilla"
 #           YYYY.MM.DD
-__date__ = "2019.08.15"
-__version__ = (2, 2, 2)
+__date__ = "2019.09.05"
+__version__ = (2, 2, 3)
 __all__ = (
     'defs', 'heuristic_deprotection', 'repl', 'tag_index', 'widgets', 'windows',
     'constants', 'core', 'crc_functions', 'exceptions', 'main',
