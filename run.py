67233ad1c88c17470c6f1994ada2f7842615b54f
Moses_of_Egypt <mosesbobadilla@gmail.com>
2020-01-12 13:14:08 -0500

    Updated minimum version

--- run.py
+++ run.py
@@ -12,9 +12,9 @@ import sys
 
 info = sys.version_info
 
-if info[0] < 3:
+if info[0] < 3 or info[1] < 5:
     input(
-        "You must have python 3 or higher installed to run Refinery.\n" +
+        "You must have python 3.5 or higher installed to run Refinery.\n"
         "You currently have %s.%s.%s installed instead." % info[:3])
     raise SystemExit(0)
 
