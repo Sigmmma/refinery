13c3b3c2629815c0e7804cc8cfbb660da06986f6
gbMichelle <gbmichelle.dev@gmail.com>
2020-02-29 16:13:59 +0100

    Move run code to main py and add a main method

--- refinery/run.py
+++ refinery/run.py
@@ -1,42 +1,19 @@
 #!/usr/bin/env python3
 #
-# This file is part of Mozzarilla.
+# This file is part of Refinery.
 #
 # For authors and copyright check AUTHORS.TXT
 #
-# Mozzarilla is free software under the GNU General Public License v3.0.
+# Refinery is free software under the GNU General Public License v3.0.
 # See LICENSE for more information.
 #
 
-import sys
+# Legacy run module, used by older MEKs
 
-info = sys.version_info
-
-if info[0] < 3 or info[1] < 5:
-    input(
-        "You must have python 3.5 or higher installed to run Refinery.\n"
-        "You currently have %s.%s.%s installed instead." % info[:3])
-    raise SystemExit(0)
-
-from datetime import datetime
-from traceback import format_exc
+from .__main__ import main
 
 try:
-    from refinery.main import Refinery
-    main_window = Refinery()
-    main_window.mainloop()
-
-except Exception:
-    exception = format_exc()
-    try:
-        main_window.log_file.write('\n' + exception)
-    except Exception:
-        try:
-            with open('startup_crash.log', 'a+') as cfile:
-                time = datetime.now().strftime("%Y-%m-%d  %H:%M:%S")
-                cfile.write("\n%s%s%s\n" % ("-"*30, time, "-"*(50-len(time))))
-                cfile.write(time + exception)
-        except Exception:
-            pass
-    print(exception)
+    main():
+    # Input was how the terminal window was kept open on Windows to show the
+    # error.
     input()
