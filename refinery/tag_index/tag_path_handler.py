8ac8402273e824397d8a5555eb53515a5c522afa
gbMichelle <gbmichelle.dev@gmail.com>
2020-03-09 00:01:21 +0100

    Fix name in license headers

--- refinery/tag_index/tag_path_handler.py
+++ refinery/tag_index/tag_path_handler.py
@@ -1,9 +1,9 @@
 #
-# This file is part of Mozzarilla.
+# This file is part of Refinery.
 #
 # For authors and copyright check AUTHORS.TXT
 #
-# Mozzarilla is free software under the GNU General Public License v3.0.
+# Refinery is free software under the GNU General Public License v3.0.
 # See LICENSE for more information.
 #
 
