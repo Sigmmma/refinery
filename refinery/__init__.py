985c289ab9b43cf66504a66bbddab2e6ff247ef9
gbMichelle <gbmichelle.dev@gmail.com>
2020-03-08 23:26:15 +0100

    Bump version

--- refinery/__init__.py
+++ refinery/__init__.py
@@ -12,8 +12,8 @@
 # ##############
 __author__ = "Devin Bobadilla, Michelle van der Graaf"
 #           YYYY.MM.DD
-__date__ = "2020.02.29"
-__version__ = (2, 4, 0)
+__date__ = "2020.03.08"
+__version__ = (2, 5, 0)
 __website__ = "https://github.com/Sigmmma/refinery"
 __all__ = (
     'defs', 'heuristic_deprotection', 'repl', 'tag_index', 'widgets', 'windows',
