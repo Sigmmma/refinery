5fa6569745f08e1a0a57f2b5ee8203d24f1dc232
gbMichelle <gbmichelle.dev@gmail.com>
2020-03-08 23:12:16 +0100

    Add a button to queue all loaded maps

--- refinery/main.py
+++ refinery/main.py
@@ -325,6 +325,9 @@ class Refinery(tk.Tk, BinillaWidget, RefineryCore):
         self.map_info_scrollbar.config(command=self.map_info_text.yview)
 
         # make the buttons
+        self.add_all_maps_button = tk.Button(
+            self.map_action_frame, text="Queue all maps",
+            command=self.queue_add_all_maps)
         self.deprotect_all_button = tk.Button(
             self.map_action_frame, text="Deprotect all",
             command=self.deprotect_all)
@@ -362,6 +365,7 @@ class Refinery(tk.Tk, BinillaWidget, RefineryCore):
         self.begin_button.pack(side='right', padx=4, pady=4)
         self.deprotect_button.pack(side='right', padx=4, pady=4)
         self.deprotect_all_button.pack(side='right', padx=4, pady=4)
+        self.add_all_maps_button.pack(side='right', padx=4, pady=4)
         self.engine_select_menu.pack(side='left', padx=4, pady=4, fill='x')
         self.map_select_menu.pack(side='left', padx=(4, 10), pady=4,
                                   fill='x', expand=True)
@@ -837,6 +841,31 @@ class Refinery(tk.Tk, BinillaWidget, RefineryCore):
             return
 
         tree_frame.activate_all()
+
+    def queue_add_all_maps(self, e=None):
+        if not self.map_loaded:
+            return
+
+        starting_map = self.active_map
+
+        for map in sorted(self.active_maps):
+            if map == ACTIVE_INDEX:
+                continue
+
+            self.set_active_map(name_or_index=map)
+
+            if self._display_mode == "hierarchy":
+                tree_frame = self.hierarchy_tree
+            elif self._display_mode == "class":
+                tree_frame = self.class_tree
+            elif self._display_mode == "hybrid":
+                tree_frame = self.hybrid_tree
+            else:
+                continue
+
+            tree_frame.activate_all()
+
+        self.set_active_map(name_or_index=starting_map)
 
     def queue_del_all(self, e=None):
         if not self.map_loaded:
